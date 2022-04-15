4fb3ed25939af741b817b99ef28d4a35dfd3b310
joseph <mmx362003@gmail.com>
2022-04-15 20:19:03 +0800

    add file

---
+++ provider/InfraStructure/NLogSetting/LayoutRenderer.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using NLog.LayoutRenderers;
+using System;
+using System.Text;
+
+namespace provider.InfraStructure.Log
+{
+    [LayoutRenderer("elapsed-time")]
+    public class ElapsedTimeLayoutRenderer : LayoutRenderer
+    {
+        private DateTime? _lastTimeStamp;
+
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var lastTimeStamp = _lastTimeStamp ?? logEvent.TimeStamp;
+            var elapsedTime = logEvent.TimeStamp - lastTimeStamp;
+            var elapsedTimeString = $"{elapsedTime.TotalSeconds:f4}";
+            builder.Append($"[{elapsedTimeString}]");
+            _lastTimeStamp = logEvent.TimeStamp;
+        }
+    }
+}
---
+++ provider/InfraStructure/NLogSetting/LogExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace provider.InfraStructure.Log
+{
+    public static class LogExtensions
+    {
+        public static void ServerLog<T>(this ILogger<T> logger, string action, string status, string subStatus, string message)
+        {
+            logger.LogInformation($"[SERVER][{action}][{status}][{subStatus}]{message}");
+        }
+        public static void PerformanceLog<T>(this ILogger<T> logger, string method, long miliSeconds, string message)
+        {
+            logger.LogInformation($"[PERFORMANCE][{method}][{miliSeconds}]{message}");
+        }
+
+        public static void EventLog<T>(this ILogger<T> logger, string method, int miliSeconds, string message)
+        {
+            logger.LogInformation($"[SERVER][{method}][{miliSeconds}]{message}");
+        }
+
+    }
+}
