af5dd1e328d7ab835e62686ca004321040b96ba8
joseph <mmx362003@gmail.com>
2022-04-22 01:03:53 +0800

    fix

--- provider/Controllers/LineBotController.cs
+++ provider/Controllers/LineBotController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using isRock.LineBot;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using provider.InfraStructure.Log;
 using provider.Model.LineBot;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace provider.Controllers
 {
@@ -17,9 +21,65 @@ namespace provider.Controllers
             _logger = ilogger;
         }
         [HttpPost]
-        public IActionResult LineBotCallBack(dynamic request)
+        public async Task<IActionResult> LineBotCallBack(dynamic request)
         {
-            return Ok(request);
+
+            try
+            {
+                string rawData = Convert.ToString(request);
+                //剖析JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(rawData);
+                var item = ReceivedMessage.events.FirstOrDefault();
+                string Message = "";
+
+                switch (item.type)
+                {
+                    case "join":
+                        Message = $"有人把我加入{item.source.type}中了，大家好啊~";
+
+                        //回覆用戶
+                        isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, _lineBotConfig.ChannelAccessToken);
+                        break;
+                    case "message":
+                        if (item.message.text == "bye")
+                        {
+                            //回覆用戶
+                            isRock.LineBot.Utility.ReplyMessage(item.replyToken, "bye-bye", _lineBotConfig.ChannelAccessToken);
+                            //離開
+                            if (item.source.type.ToLower() == "room")
+                                isRock.LineBot.Utility.LeaveRoom(item.source.roomId, _lineBotConfig.ChannelAccessToken);
+                            if (item.source.type.ToLower() == "group")
+                                isRock.LineBot.Utility.LeaveGroup(item.source.roomId, _lineBotConfig.ChannelAccessToken);
+
+                            break;
+                        }
+                        Message = "你說了:" + ReceivedMessage.events[0].message.text;
+                        //取得用戶名稱 
+                        LineUserInfo UserInfo = null;
+                        if (item.source.type.ToLower() == "room")
+                            UserInfo = isRock.LineBot.Utility.GetRoomMemberProfile(
+                                item.source.roomId, item.source.userId, _lineBotConfig.ChannelAccessToken);
+                        if (item.source.type.ToLower() == "group")
+                            UserInfo = isRock.LineBot.Utility.GetGroupMemberProfile(
+                                item.source.groupId, item.source.userId, _lineBotConfig.ChannelAccessToken);
+                        //顯示用戶名稱
+                        if (item.source.type.ToLower() != "user")
+                            Message += "\n你是:" + UserInfo.displayName;
+
+                        //回覆用戶
+                        isRock.LineBot.Utility.ReplyMessage(item.replyToken, Message, _lineBotConfig.ChannelAccessToken);
+                        break;
+                    default:
+                        break;
+                }
+                //回覆API OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                //todo:請自行處理exception
+                return Ok();
+            }
         }
 
         [HttpGet]
