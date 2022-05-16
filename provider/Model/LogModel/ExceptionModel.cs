39d81073e35ac61b54394a1d21444da3da1a0c05
joseph <mmx362003@gmail.com>
2022-05-16 09:26:28 +0800

    Add file

---
+++ provider/Model/LogModel/ExceptionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace provider.Model.Log
+{
+    public class ExceptionModel
+    {
+        public string Location { get; set;  }
+        public string StackTrace { get; set; }
+        public string Message { get; set; }
+        public string Name { get; set; }
+    }
+}
